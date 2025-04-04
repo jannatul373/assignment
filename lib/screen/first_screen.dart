67aae5b7e1d0c337c868ae7cf4e65f75a8ec691c
jannatul373 <ferdaus15-4982@diu.edu.bd>
2025-04-04 16:01:53 +0600

    firstPage

--- lib/screen/first_screen.dart
+++ lib/screen/first_screen.dart
@@ -6,12 +6,13 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("First Screen")),
+      appBar: AppBar(title: const Text("Navigation")),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             Navigator.pushNamed(context, '/second');
           },
+          child: const Text("Go  back"),
         ),
       ),
     );
